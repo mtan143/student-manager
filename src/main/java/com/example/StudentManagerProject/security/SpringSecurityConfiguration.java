105b40e62a5691b56c50fa7b01e64a935ca255ca
mtan143 <nguyentantnnn82@gmail.com>
2021-12-13 21:50:40 +0700

    change something

--- src/main/java/com/example/StudentManagerProject/security/SpringSecurityConfiguration.java
+++ src/main/java/com/example/StudentManagerProject/security/SpringSecurityConfiguration.java
@@ -1,64 +1,64 @@
-package com.example.StudentManagerProject.security;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.DebugBeanDefinitionParser;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Configuration
-@EnableWebSecurity
-public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Bean
-    AuthenticationProvider authenticationProvider() {
-
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-
-        provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(new BCryptPasswordEncoder());
-
-        return provider;
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .cors()
-                .and()
-                .csrf()
-                .disable()
-                .authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/error/**").permitAll()
-                .antMatchers("/classes/**").hasAnyAuthority("ADMIN", "TEACHER")
-                .antMatchers("/scoreboards/**").hasAnyAuthority("ADMIN", "TEACHER")
-                .antMatchers(HttpMethod.GET, "/scoreboards/**").hasAuthority("STUDENT")
-                .antMatchers("/semesters/**").hasAuthority("ADMIN")
-                .antMatchers("/semester-scores/**").hasAnyAuthority("ADMIN", "TEACHER")
-                .antMatchers("/students/**").hasAnyAuthority("ADMIN", "TEACHER")
-                .antMatchers("/subjects/**").hasAuthority("ADMIN")
-                .antMatchers("/teachers/**").hasAuthority("ADMIN")
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
-    }
-}
+//package com.example.StudentManagerProject.security;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.AuthenticationProvider;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.DebugBeanDefinitionParser;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
+//
+//    @Autowired
+//    private UserDetailsService userDetailsService;
+//
+//    @Bean
+//    AuthenticationProvider authenticationProvider() {
+//
+//        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+//
+//        provider.setUserDetailsService(userDetailsService);
+//        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+//
+//        return provider;
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .cors()
+//                .and()
+//                .csrf()
+//                .disable()
+//                .authorizeRequests()
+//                .antMatchers("/").permitAll()
+//                .antMatchers("/error/**").permitAll()
+//                .antMatchers("/classes/**").hasAnyAuthority("ADMIN", "TEACHER")
+//                .antMatchers("/scoreboards/**").hasAnyAuthority("ADMIN", "TEACHER")
+//                .antMatchers(HttpMethod.GET, "/scoreboards/**").hasAuthority("STUDENT")
+//                .antMatchers("/semesters/**").hasAuthority("ADMIN")
+//                .antMatchers("/semester-scores/**").hasAnyAuthority("ADMIN", "TEACHER")
+//                .antMatchers("/students/**").hasAnyAuthority("ADMIN", "TEACHER")
+//                .antMatchers("/subjects/**").hasAuthority("ADMIN")
+//                .antMatchers("/teachers/**").hasAuthority("ADMIN")
+//                .anyRequest().authenticated()
+//                .and()
+//                .httpBasic();
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.authenticationProvider(authenticationProvider());
+//    }
+//}
